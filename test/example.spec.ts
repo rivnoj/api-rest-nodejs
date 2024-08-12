4f2af454fd319753dcfa8c415ed064ea05cd1abe
rivnoj <84639053+rivnoj@users.noreply.github.com>
2024-08-12 14:48:21 -0300

    Testando criação de transação

--- test/example.spec.ts
+++ test/example.spec.ts
@@ -1,9 +1,22 @@
-import { expect, test } from 'vitest'
+import { test, beforeAll, afterAll } from 'vitest'
+import request from 'supertest'
+import { app } from '../src/app'
 
-test('o usuário consegue criar uma nova transação', () => {
-  // chamada HTTP para criar uma nova transação
-  // validação
-  const responseStatusCode = 201
+beforeAll(async () => {
+  await app.ready()
+})
 
-  expect(responseStatusCode).toEqual(201)
+afterAll(async () => {
+  await app.close()
+})
+
+test('user can create a new transaction', async () => {
+  await request(app.server)
+    .post('/transactions')
+    .send({
+      title: 'New transaction',
+      amount: 5000,
+      type: 'credit',
+    })
+    .expect(201)
 })
